08f29b78d8a1659b6f630c695b7532f9f3bc73c3
Uriel-229 <urielgodonou7@gmail.com>
2022-07-02 21:39:01 -0700

    clear

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
 /**
- * function - main
- * description - the starting of the program
+ * Program - This is a description
  */
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle\n");
+puts("Programming is like building a multilingual puzzle\n);
 return (0);
 }
