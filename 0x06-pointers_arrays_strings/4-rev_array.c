4d63be5d50ba8ef6ddd6348d75d3d552f2e5022e
Uriel-229 <urielgodonou7@gmail.com>
2022-10-27 19:56:02 -0700

    4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * reserve_array - reserves an array of integer
+ * @a: arrays
+ * @n: number of element of the array
+ * Return: rev
+ */
+void reserve_array(int *a, int n)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < n; i++)
+	{
+		n--;
+		j = a[i];
+		a[i] = a[n];
+		a[n] = j;
+	}
+}
